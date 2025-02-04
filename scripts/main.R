338baace6404d430df00791482a8f6a9dc15835f
Taavi Päll <tapa741@gmail.com>
2025-02-04 13:56:45 +0200

    updated main.R script

--- scripts/main.R
+++ scripts/main.R
@@ -5,7 +5,7 @@
 #' ---
 
 #+ include=FALSE
-knitr::opts_chunk$set(message = FALSE)
+knitr::opts_chunk$set(echo = FALSE, message = FALSE)
 
 #+
 library(tidyverse)
@@ -25,16 +25,15 @@ weekly_responses <- weekly_responses_files %>%
   ) %>% 
   select(type, start_date, end_date, participantID, starts_with("weekly"))
 
-weekly_si <- read_csv(here("output/survey_info_weekly.csv"))
+weekly_si <- read_csv(here("data/survey_info_weekly.csv"))
 
 
 #' ## Active users
 #' 
-weekly_responses %>% 
-  distinct(participantID) %>% 
-  count()
 
+#' Total number of unique user IDs: `r n_distinct(weekly_responses$participantID)`
 
+#+
 weekly_responses %>% 
   group_by(start_date) %>% 
   distinct() %>% 
@@ -43,7 +42,7 @@ weekly_responses %>%
   geom_line(aes(start_date, n)) +
   scale_y_continuous("Active Users", limits = c(0, NA)) +
   scale_x_date("Date", date_breaks = "month", date_labels = "%b %Y") +
-  labs(title = "Gripiradar")
+  labs(title = "Gripiradar \u2014 Active Users")
 
 #' ## Symptoms
 #' 
@@ -64,10 +63,10 @@ weekly_responses %>%
   ) %>% 
   ggplot(aes(value, fct_reorder(label, value))) +
   geom_col() +
-  facet_wrap(~int) +
+  facet_wrap(~str_replace_all(str_remove_all(as.character(int), "UTC"), "--", "\u2014 ")) +
   scale_y_discrete("Symptoms") +
   scale_x_continuous("Frequency (%)", labels = scales::percent) +
-  labs(title = "Gripiradar")
+  labs(title = "Gripiradar \u2014 Symptoms")
 
 #' ## Demograpics
 intake_responses_files <- list.files(here(), recursive = TRUE, full.names = TRUE, pattern = "intake_responses")
@@ -82,7 +81,7 @@ intake_responses <- intake_responses_files %>%
   ) %>% 
   select(type, start_date, end_date, participantID, starts_with("intake"))
 
-intake_si <- read_csv(here("output/survey_info_intake.csv"))
+intake_si <- read_csv(here("data/survey_info_intake.csv"))
 demographics <- intake_responses %>% 
   mutate(int = interval(start_date, end_date)) %>% 
   select(int, end_date, participantID, female = intake.Q1, birth_date = intake.Q2, zip_code = intake.Q3.0) %>% 
@@ -98,20 +97,22 @@ demographics %>%
   geom_text(aes(label = after_stat(count)), stat = "count", vjust = 1.5, color = "white") +
   scale_y_continuous("Frequency (%)", labels = scales::percent) +
   theme(axis.title.x = element_blank()) +
-  labs(title = "Gripiradar")
+  labs(title = "Gripiradar \u2014 Gender")
 
 
 #' ### Zip codes
-adr <- read_csv(here("output/zip_codes.csv")) %>% 
+adr <- read_csv(here("data/zip_codes.csv")) %>% 
   select(zip_code, county) %>% 
   distinct()
 
-demographics %>% 
-  select(participantID, zip_code) %>% 
-  distinct() %>% 
+#+ include=FALSE
+demographics %>%
+  select(participantID, zip_code) %>%
+  distinct() %>%
   count(`Sihtnumber olemas` = !is.na(zip_code))
 n_distinct(demographics$participantID)
 
+#+
 demographics %>% 
   select(participantID, zip_code) %>% 
   left_join(adr) %>% 
@@ -121,9 +122,11 @@ demographics %>%
   geom_text(aes(label = after_stat(count)), stat = "count", hjust = 1.2, color = "white") +
   scale_x_continuous("Frequency (%)", labels = scales::percent) +
   scale_y_discrete("County") +
-  labs(title = "Gripiradar")
+  labs(title = "Gripiradar \u2014 Counties")
 
 #' ### Age groups
+
+#+ include=FALSE
 demographics %>% 
   select(participantID, age) %>% 
   distinct() %>% 
@@ -139,6 +142,7 @@ demographics %>%
   ) %>% 
   count(age_group)
 
+#+
 demographics %>% 
   select(participantID, age) %>% 
   distinct() %>% 
@@ -157,4 +161,4 @@ demographics %>%
   geom_text(aes(label = after_stat(count)), stat = "count", hjust = 1.2, color = "white") +
   scale_x_continuous("Frequency (%)", labels = scales::percent) +
   scale_y_discrete("Age group") +
-  labs(title = "Gripiradar")
+  labs(title = "Gripiradar \u2014 Age Groups")
