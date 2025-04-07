a3bfeedee055b3c48043753ffaa8a712c12e4ae9
Taavi Päll <tapa741@gmail.com>
2025-04-07 11:12:33 +0300

    updated multilanguage rendering

--- scripts/render.R
+++ scripts/render.R
@@ -1,24 +1,18 @@
 library(minioclient)
 library(quarto)
 
-#' Pst!, don't tell to Jenny Bryan
 setwd("work")
 
 #' Downloading data from gripiradar web
+message("Downloading data")
 mc_alias_set(
-  "gripiradar", 
-  "s3.hpc.ut.ee", 
-  access_key = Sys.getenv("GRIPIRADAR_ACCESS_KEY"), 
+  "gripiradar",
+  "s3.hpc.ut.ee",
+  access_key = Sys.getenv("GRIPIRADAR_ACCESS_KEY"),
   secret_key = Sys.getenv("GRIPIRADAR_SECRET_KEY")
 )
 mc_cp("gripiradar/gripiradar-ut-ee", ".", recursive = TRUE)
 
 #' Rendering gripiradar dashboards in three languages
-for (lang in c("ee", "en", "ru")) {
-  message("Starting to render")
-  quarto_render(
-    "gripiradar.qmd", 
-    execute_params = list("lang" = lang), 
-    output_file = paste0("gripiradar_", lang, ".html")
-    )
-}
+message("Starting to render")
+quarto_render("index.qmd")
