4a0748d507aeea7b78a25617dc5c712e172a9025
Taavi Päll <tapa741@gmail.com>
2025-05-12 12:49:51 +0300

    updated index.qmd, removed header

--- render.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-  -e GRIPIRADAR_ACCESS_KEY=$GRIPIRADAR_ACCESS_KEY \
-  -e GRIPIRADAR_SECRET_KEY=$GRIPIRADAR_SECRET_KEY \
-  -v $(pwd):/work \
-  taavipall/gripiradar \
-  Rscript /work/scripts/render.R
-rm -rf gripiradar-ut-ee
